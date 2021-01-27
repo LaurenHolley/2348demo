f3a87bf93a007757f80e79c3125bb572fb1c1c8d
LaurenHolley <77747367+LaurenHolley@users.noreply.github.com>
2021-01-27 14:56:23 -0600

    Add files via upload

---
+++ HW1/HW1Zy120.py
@@ -0,0 +1,11 @@
+# Lauren Holley
+# 1861058
+user_num = int(input("Enter integer:"))
+print()
+print("You entered:", user_num)
+print(user_num, "squared is", user_num * user_num)
+print("And", user_num, "cubed is", user_num ** 3, "!!")
+user_num1 = int(input("Enter another integer:"))
+print()
+print(user_num, "+", user_num1, "is", user_num + user_num1)
+print(user_num, "*", user_num1, "is", user_num * user_num1)
