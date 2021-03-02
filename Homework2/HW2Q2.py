8ef2a0e6a9fb5eb9d56244941affdca8556f092a
LaurenHolley <77747367+LaurenHolley@users.noreply.github.com>
2021-03-02 11:35:23 -0600

    Add files via upload
    This final commit includes adding a write file named 'parsedDates.txt'

--- Homework2/HW2Q2.py
+++ Homework2/HW2Q2.py
@@ -3,7 +3,6 @@
 from datetime import date as dt
 import datetime
 
-
 f = open('inputDates.txt', 'r')
 date_line = f.read()
 date_format = "%B %d, %Y"
@@ -15,10 +14,8 @@ for date in list(individual_date):
         correct_date.append(date)
     except ValueError:
         continue
-
-month_dict = {'January' : 1, 'February' : 2, 'March' : 3, 'April' : 4, 'May' : 5,
-'June' : 6, 'July' : 7, 'August' : 8, 'September' : 9, 'October' : 10,
-'November' : 11, 'December' : 12 }
+month_dict = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 'August': 8,
+              'September': 9, 'October': 10, 'November': 11, 'December': 12}
 
 list_date = []
 for date in correct_date:
@@ -27,14 +24,18 @@ for date in correct_date:
     num_month = month_dict[month_name]
     day = date[month_index+1:-6]
     year = date[-4:]
-    new_date_string = ('{}/{}/{}'.format(num_month,day,year))
+    new_date_string = ('{}/{}/{}'.format(num_month, day, year))
     list_date.append(new_date_string)
 
 today = datetime.datetime.today()
 current_date = today.strftime('%m/%d/%Y')
 
+final_date = []
 for date in list_date:
-    if datetime.datetime.strptime(current_date,'%m/%d/%Y') > datetime.datetime.strptime(date,'%m/%d/%Y'):
-        print(date)
+    if datetime.datetime.strptime(current_date, '%m/%d/%Y') > datetime.datetime.strptime(date, '%m/%d/%Y'):
+        file_new = open('parsedDates.txt', 'w')
+        final_date.append('{} \n'.format(date))
+        file_new.writelines(final_date)
+        file_new.close()
     else:
         continue
